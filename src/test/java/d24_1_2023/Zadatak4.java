bd69c43ce1dfc8b234a0f3b8b3348ea56f6e1c42
Luka Andrejevic <lukaandrejevic88@gmail.com>
2023-01-26 16:15:22 +0100

    domaci

---
+++ src/test/java/d24_1_2023/Zadatak4.java
@@ -0,0 +1,33 @@
+package d24_1_2023;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Zadatak4 {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://artplayer.org/");
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//i[@aria-label='Play']")).click();
+        driver.findElement(By.xpath("//i[@aria-label='Mute']")).click();
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//div[@aria-label='Screenshot']/i")).click();
+        driver.findElement(By.xpath("//div[@aria-label='PIP Mode']/i")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//div[@aria-label='Exit PIP Mode']/i")).click();
+        driver.findElement(By.xpath("//div[@aria-label='Web Fullscreen']/i")).click();
+        driver.findElement(By.xpath("//div[@aria-label='Exit Web Fullscreen']/i[2]")).click();
+        Thread.sleep(5000);
+
+        driver.quit();
+
+    }
+}
